8f09522b166eb69d47c5e1fe3b41742b2c028564
denverdino <denverdino@gmail.com>
2014-02-04 10:28:54 +0800

    Fix the JS format

--- www/location.js
+++ www/location.js
@@ -1,33 +1,30 @@
-cordova.define("com.yili.phonegap.plugins.BaiduLocation", function(require, exports, module) {
-	var location = {
-	    execute: function(action, successCallback, errorCallback) {
-	    	cordova.exec(    
-	    		function(pos) {
-					var errcode = pos.code;
-					if (errcode == 61 || errcode == 65 || errcode == 161) {
-						successCallback(pos);
-					} else {
-						if (typeof(errorCallback) != "undefined") {
-							if (errcode >= 162) {
-								errcode = 162;
-							}
-							errorCallback(code[errcode])
-						};
-					}
-	    		}, 
-	    		function(err){},
-	    		"BaiduLocationPlugin",
-	    		action,
-	    		[]
-	    	)
-	    },
-	    get: function(successCallback, errorCallback) {
-	    	this.execute("get", successCallback, errorCallback);
-	    },
-	    stop: function(action, successCallback, errorCallback) {
-	    	this.execute("stop", successCallback, errorCallback);
-	    }
+var location = {
+	execute: function(action, successCallback, errorCallback) {
+		cordova.exec(    
+			function(pos) {
+				var errcode = pos.code;
+				if (errcode == 61 || errcode == 65 || errcode == 161) {
+					successCallback(pos);
+				} else {
+					if (typeof(errorCallback) != "undefined") {
+						if (errcode >= 162) {
+							errcode = 162;
+						}
+						errorCallback(code[errcode])
+					};
+				}
+			}, 
+			function(err){},
+			"BaiduLocationPlugin",
+			action,
+			[]
+		)
+	},
+	get: function(successCallback, errorCallback) {
+		this.execute("get", successCallback, errorCallback);
+	},
+	stop: function(action, successCallback, errorCallback) {
+		this.execute("stop", successCallback, errorCallback);
 	}
 }
 module.exports = location;
-});
